4d68aacfad0dd9f268485264af4dcd561ef3685c
Velvett Gutierrez <147103300+GutierrezVelvett@users.noreply.github.com>
2026-05-13 19:50:59 +0000

    errortres

--- main.tf
+++ main.tf
@@ -15,14 +15,14 @@ data "aws_ami" "app_ami" {
 }
 
 resource "aws_vpc" "default"{
-  default=true
+  default = true
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
