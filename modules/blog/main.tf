86c952e7ac2e1c49649a7906ec55a57243af85e5
Velvett Gutierrez <147103300+GutierrezVelvett@users.noreply.github.com>
2026-05-14 04:54:30 +0000

    deploy qa

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -35,7 +35,7 @@ module "blog_vpc" {
 module "blog_sg" {
 source  = "terraform-aws-modules/security-group/aws"
 version = "5.3.1"
-name = "blog_new"
+name = "${var.environment.name}-blog"
 
 vpc_id = module.blog_vpc.vpc_id
 ingress_rules = ["http-80-tcp","https-443-tcp"]
@@ -49,7 +49,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -80,7 +80,7 @@ module "blog_alb" {
 }
 
 resource "aws_lb_target_group" "blog" {
-  name     = "blog"
+  name     = "${var.environment.name}-blog"
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
@@ -90,7 +90,7 @@ module "blog_autoscaling" {
 source  = "terraform-aws-modules/autoscaling/aws"
 version = "9.2.1"
 
-name="blog"
+name="${var.environment.name}-blog"
 
 min_size = var.min_size
 max_size = var.max_size
@@ -103,7 +103,7 @@ instance_type          = var.instance_type
 image_id               = data.aws_ami.app_ami.id
 
 traffic_source_attachments = {
-  blog-alb ={
+  ${var.environment.name}-blog-alb ={
     traffic_source_identifier = aws_lb_target_group.blog.arn
   }
 }
